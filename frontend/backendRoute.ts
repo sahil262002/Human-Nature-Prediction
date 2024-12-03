5a56875151b06d5c3ff7f58dcf53767ff43a25a9
sahil <skr.kis2002@gmail.com>
2024-12-03 19:38:08 +0530

    deploying

---
+++ frontend/backendRoute.ts
@@ -0,0 +1,11 @@
+export const links ={
+    signup : "https://backend.skr-kis2002.workers.dev/api/v1/user/signup",
+    signin : "https://backend.skr-kis2002.workers.dev/api/v1/user/signin",
+    quest : "https://backend.skr-kis2002.workers.dev/api/v1/quest/question",
+    check : "https://backend.skr-kis2002.workers.dev/api/v1/quest/check",
+    userProfile: "https://backend.skr-kis2002.workers.dev/api/v1/user-profile/id",
+    userPassword:"https://backend.skr-kis2002.workers.dev/api/v1/user-profile/change",
+    userPassChange: "https://backend.skr-kis2002.workers.dev/api/v1/user-profile/changepassword",
+    logout: "https://backend.skr-kis2002.workers.dev/api/v1/user/logout",
+
+}
