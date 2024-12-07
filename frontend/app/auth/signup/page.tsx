064ec8e2b6e469198ddfa3a264db83908f3ee46b
Rajveer kumar <sharmarajveer8287631167@gmail.com>
2024-12-07 19:38:24 +0530

    Update page.tsx

--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { links } from '../../../backendRoute';
+import { links } from "@/backendRoute";
 import { ButtonBack } from "@/components/ButtonBack";
 import { Loading } from "@/components/Loading";
 import { useState } from "react";
-
-
 
 export default function Signup() {
   const [password, setPassword] = useState<string>();
@@ -22,7 +20,7 @@ export default function Signup() {
     return (
       <>
         <div className="text-center flex-col content-center w-screen h-[calc(100vh-4rem)]">
-          <div className=" flex justify-center">
+          <div className="flex justify-center">
             <Loading />
           </div>
         </div>
@@ -32,10 +30,14 @@ export default function Signup() {
 
   return (
     <>
-    
-      <div className="text-center flex-col content-center w-screen h-[calc(100vh-4rem)]">
-        <div className=" flex justify-center">
-          <div className="border-solid border-2 border-slate-600 rounded-lg border-t-4  shadow-indigo-500/50  shadow-2xl">
+      <div
+        className="flex flex-col items-center justify-center w-full h-[calc(100vh-4rem)] bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/bg.jpg')", // Path to your background image
+        }}
+      >
+        <div className="flex justify-center w-full">
+          <div className="border-solid border-2 border-slate-800 rounded-lg border-t-4 shadow-indigo-500/50 shadow-2xl max-w-xs w-full p-8 bg-transparent">
             <div className="m-3">
               <div>
                 <input
@@ -43,9 +45,7 @@ export default function Signup() {
                   type="text"
                   placeholder="name"
                   value={name}
-                  onChange={(e) =>
-                    setName(() => e.target.value)
-                  }
+                  onChange={(e) => setName(() => e.target.value)}
                 />
               </div>
               <div>
@@ -54,20 +54,16 @@ export default function Signup() {
                   type="email"
                   placeholder="email"
                   value={email}
-                  onChange={(e) =>
-                    setEmail(() => e.target.value)
-                  }
+                  onChange={(e) => setEmail(() => e.target.value)}
                 />
               </div>
               <div className="mt-2">
                 <input
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   type="password"
                   placeholder="password"
                   value={password}
-                  onChange={(e) =>
-                    setPassword(() => e.target.value)
-                  }
+                  onChange={(e) => setPassword(() => e.target.value)}
                 />
               </div>
               <div className="mt-2">
